4d7cf81a588ab60c59ff4a5e68982aeabb104614
lenok97 <iamhappluck@gmail.com>
2017-11-11 22:41:35 +0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -1,17 +1,67 @@
-from wsgiref import simple_server, validate
-from jinja2 import Environment, FileSystemLoader
+from webob import Request
+from paste.httpserver import serve
+import os
 
-class TrivialWSGIServer(object)
-    def __init__(self, app)
+current_directory = os.getcwd()
+
+#WSGI middleware которое будет вставлять в HTML документ JavaScript и CSS файлы
+class WsgiMiddleware(object):
+    includes = [
+    'app.js',
+    'react.js',
+    'leaflet.js',
+    'D3.js',
+    'moment.js',
+    'math.js',
+    'main.css',
+    'bootstrap.css',
+   'normalize.css',
+   ]
+    
+    css = ""
+    js = ""
+    
+    def __init__(self, app):
         self.app = app
-        self.server = simple_server.WSGIServer(
-            ('localhost', 8000),
-            simple_server.WSGIRequestHandler,
-        )
-        self.server.set_app(validate.validator(self.app))
+    
+    def __call__(self, environ, start_response):            
+        WsgiMiddleware.sort_includes()
+        try:
+            response = self.app(environ, start_response).decode() 
+            if response.find('<head>') >=0:
+                data, headend = response.split('</head>')
+                response = data + WsgiMiddleware.css + '</head>' + headend
+            if response.find('<body>') >=0:
+                data, htmlend = response.split('</body>')
+                response = data + WsgiMiddleware.js +'</body>' + htmlend
+            yield (response).encode()  
+        except FileNotFoundError:
+            response_code = '404 Not Found'
+            response_type = ('Content-Type', 'text/HTML')
+            start_response(response_code, [response_type])
+            yield ("404 Not Found").encode()
 
-    def serve(self)
-        self.server.serve_forever()
+    #разделим includes на две категории
+    def sort_includes():
+        for include in WsgiMiddleware.includes:
+            if(include.split('.')[1] == 'js'):
+                WsgiMiddleware.js += '<script src="/_static/' + include + '"></script>\n'
+            else:
+                WsgiMiddleware.css += '<link rel="stylesheet" href="/_static/' + include + '"/>\n'
 
-def runner(app)
-    TrivialWSGIServer(app).serve()
+#приложение которое отдает статикой файлы index.html и about.html
+#(about.html доступно по ссылке из index.html)
+def give_static(environ, start_response):
+    path = '\index.html'
+    file= open(current_directory+path, 'r')
+    response_code = '200 OK'
+    response_type = ('Content-Type', 'text/HTML')
+    start_response(response_code, [response_type])
+    data=file.read() # str to bytes
+    return data.encode()
+
+# Оборачиваем WSGI приложение в middleware
+give_static = WsgiMiddleware(give_static)
+
+# Запускаем сервер
+serve(give_static, host='localhost', port=8000)
